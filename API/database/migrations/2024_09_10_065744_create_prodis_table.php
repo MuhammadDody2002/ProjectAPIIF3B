78f2cb97ebb914cbf127bafb906ab205fe334e4d
MuhammadDody2002 <muhammaddody05@mhs.mdp.ac.id>
2024-09-10 15:10:35 +0700

    update

---
+++ API/database/migrations/2024_09_10_065744_create_prodis_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Fakultas;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prodis', function (Blueprint $table) {
+           $table->uuid('id');
+            $table->primary('id');
+            $table->string('nama', 50);
+            $table->uuid('fakultas_id'); // Add this line to create the fakultas_id column
+            $table->foreign('fakultas_id')->references('id')->on('fakultas')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prodis');
+    }
+};
